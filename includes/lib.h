58898468050b8c723292903a15364e40f85e4c2e
Ryan Zhiyang Tan <ryatan@c2r2s4.42singapore.sg>
2025-10-27 22:32:19 +0800

    norminetted helper and lib.h

--- includes/lib.h
+++ includes/lib.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 12:46:55 by ryatan            #+#    #+#             */
-/*   Updated: 2025/10/27 22:03:06 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/10/27 22:31:15 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <fcntl.h>
 # include <stdlib.h>
 
-typedef struct	s_coord_len 
+typedef struct s_coord_len
 {
 	int	x;
 	int	y;
@@ -27,20 +27,19 @@ typedef struct	s_coord_len
 // helpers
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
-int	ft_strlen(char *s);
+int		ft_strlen(char *s);
 
 // parsing
 void	ft_read_into_array(char ***array, char *str, int row, int buffer);
-int	ft_firstline(const char *file_path);
-int	ft_filebuffer(const char *file_path);
 char	**ft_read_txt_file(const char *file_path, char **map);
+int		ft_firstline(const char *file_path);
+int		ft_filebuffer(const char *file_path);
 
 // solving
+t_coord	ft_find_square(char **map);
 void	free_cache(int **cache, int cols);
-int	**ft_create_cache(int rows, int cols);
 void	ft_init_cache(int **cache, int rows, int cols);
 void	ft_cache(int **cache, int row_value, int col_value, int biggest_length);
-int	ft_min_of_three(int i, int j, int k)
-t_coord	ft_find_square(char **map);
-
+int		**ft_create_cache(int rows, int cols);
+int		ft_min_of_three(int i, int j, int k);
 #endif
